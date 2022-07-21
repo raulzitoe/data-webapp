ff8bb8cb306009c60c7eb4a86a00362c95e166b3
raulzitoe <raulguil@gmail.com>
2022-07-21 12:48:53 -0400

    add home route, basic home layout

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import GraphScreen from "./routes/GraphScreen";
 import TableScreen from "./routes/TableScreen";
-import MapScreen from './routes/MapScreen';
+import MapScreen from "./routes/MapScreen";
+import HomeScreen from "./routes/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/eq-works-webapp" element={<App />} >
+      <Route path="/eq-works-webapp" element={<App />}>
+        <Route index element={<HomeScreen />} />
         <Route path="/eq-works-webapp/graph" element={<GraphScreen />} />
         <Route path="/eq-works-webapp/table" element={<TableScreen />} />
         <Route path="/eq-works-webapp/map" element={<MapScreen />} />
       </Route>
     </Routes>
-    
   </BrowserRouter>
 );
 
---
+++ src/routes/HomeScreen.jsx
@@ -0,0 +1,9 @@
+function HomeScreen() {
+  return (
+    <div className="bg-primary" style={{height: '95vh'}} >
+      <h1>HOME SCREEN</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
