422c1b64593a5ac3ab4a0c02ec85f0c380bbde70
raulzitoe <raulguil@gmail.com>
2022-07-25 13:28:39 -0400

    move navbar to components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import logo from "../src/logo.svg";
+import logo from "../../src/logo.svg";
 
 function NavBar() {
   return (
