1eeb6eda739949353d588a76d61f268b4e985c8e
raulzitoe <raulguil@gmail.com>
2022-07-26 13:39:56 -0400

    poi field show poi name instead of id, clean code

--- src/routes/tablescreen.jsx
+++ src/routes/tablescreen.jsx
@@ -1,24 +1,58 @@
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
-import { useGlobalFilter, useTable } from "react-table";
+import { useGlobalFilter, useSortBy, useTable } from "react-table";
 import { Table, Card } from "react-bootstrap";
 import { GlobalFilter } from "../components/GlobalFilter";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import moment from "moment";
+import Spinner from "react-bootstrap/Spinner";
 
-export default function TableScreen(props) {
+export default function TableScreen() {
   const [events, setEvents] = useState([]);
+  const [startDate, setStartDate] = useState(new Date("2017-01-01"));
+  const [endDate, setEndDate] = useState(new Date("2017-01-15"));
+  const [poi, setPoi] = useState([]);
 
-  const fetchEvents = async () => {
-    const response = await axios
-      .get("https://gelatinous-crystalline-guppy.glitch.me/events/hourly")
-      .catch((err) => console.log(err));
+  useEffect(() => {
+    const fetchPoi = async () => {
+      const response = await axios
+        .get("https://gelatinous-crystalline-guppy.glitch.me/poi")
+        .catch((err) => console.log(err));
+  
+      if (response) {
+        const poi = response.data;
+  
+        console.log("Poi: ", poi);
+        setPoi(poi);
+      }
+    };
+    fetchPoi();
+  }, []);
 
-    if (response) {
-      const events = response.data;
-
-      console.log("Events: ", events);
-      setEvents(events);
-    }
-  };
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await axios
+        .get(
+          `https://gelatinous-crystalline-guppy.glitch.me/events/hourly/${moment(
+            startDate
+          ).format("YYYY-MM-DD")}/${moment(endDate).format("YYYY-MM-DD")}`
+        )
+        .catch((err) => console.log(err));
+  
+      if (response) {
+        const events2 = response.data;
+        events2.map((x)=>(x.poi_id = poi.find((y) => y.poi_id === x.poi_id)?.name    ));
+        setEvents(events2);
+  
+        console.log("Events: ", events2);
+      }
+    };
+    fetchEvents();
+  }, [startDate, endDate, poi]);
 
   const eventsData = useMemo(() => [...events], [events]);
 
@@ -31,11 +65,11 @@ export default function TableScreen(props) {
         : [],
     [events]
   );
-  console.log(events);
-
+  
   const tableInstance = useTable(
     { columns: eventsColumns, data: eventsData },
-    useGlobalFilter
+    useGlobalFilter,
+    useSortBy
   );
 
   const {
@@ -49,51 +83,106 @@ export default function TableScreen(props) {
     state,
   } = tableInstance;
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
-
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  return (
-    <div className="bg-primary pt-5 pb-5 p-lg-5">
-      <Card className="shadow-lg p-0 p-lg-5 mx-auto">
-        <h2 className="mx-auto mb-2 mt-2 mb-lg-5">Table Screen</h2>
-        <GlobalFilter
-          preGlobalFilteredRows={preGlobalFilteredRows}
-          setGlobalFilter={setGlobalFilter}
-          globalFilter={state.globalFilter}
-        />
-        <Table {...getTableProps()} reponsive="true" striped hover size="sm bg-white">
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()} className="ps-3">
-                    {capitalizeFirstLetter(column.render("Header"))}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  })}
+  const handleStartDateChange = (newValue) => {
+    setStartDate(newValue);
+  };
+
+  const handleEndDateChange = (newValue) => {
+    setEndDate(newValue);
+  };
+
+  if (events && poi) {
+    return (
+      <div className="bg-primary pt-5 pb-5 p-lg-5">
+        <Card className="shadow-lg p-0 p-lg-5 mx-auto">
+          <h2 className="mx-auto mb-2 mt-2 mb-lg-4">Table Screen</h2>
+          <div className="w-50 mx-auto mb-4">
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+              <Stack
+                direction={{
+                  xs: "column",
+                  sm: "column",
+                  md: "row",
+                  lg: "row",
+                  xl: "row",
+                }}
+                spacing={2}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <DesktopDatePicker
+                  label="Start"
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+                <DesktopDatePicker
+                  label="End"
+                  value={endDate}
+                  onChange={handleEndDateChange}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </Stack>
+            </LocalizationProvider>
+          </div>
+          <GlobalFilter
+            preGlobalFilteredRows={preGlobalFilteredRows}
+            setGlobalFilter={setGlobalFilter}
+            globalFilter={state.globalFilter}
+          />
+          <Table
+            {...getTableProps()}
+            reponsive="true"
+            striped
+            hover
+            size="sm bg-white"
+          >
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      className="ps-3"
+                    >
+                      {capitalizeFirstLetter(column.render("Header"))}
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " ▼"
+                          : " ▲"
+                        : ""}
+                    </th>
+                  ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </Card>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => {
+                      return (
+                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mx-auto m-5">
+      <Spinner animation="border" variant="primary" />
     </div>
   );
 }
