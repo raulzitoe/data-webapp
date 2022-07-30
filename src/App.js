3c0f05744a36a776c6b59580a6dbd350fb8cb53e
raulzitoe <raulguil@gmail.com>
2022-07-30 13:01:32 -0400

    add react-query dependencies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <NavBar />
       <Outlet />
-    </>
+    </QueryClientProvider>
   );
 }
 
